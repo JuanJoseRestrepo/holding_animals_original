3089b66774b9a3a9e6a59e39823592a5edd15a29
Usuario de Salon <aulas@academico.icesi.edu.co>
2019-09-11 02:41:04 -0500

    finish the binary

--- holding_animals_1_1-master/holding_animals1/test/test/testClub.java
+++ holding_animals_1_1-master/holding_animals1/test/test/testClub.java
@@ -4,8 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import model.Club;
 class testClub {
 
+	
+	private Club b;
+	
+	private void setupEscenario() {
+		b = new Club("","","","");
+	}
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
